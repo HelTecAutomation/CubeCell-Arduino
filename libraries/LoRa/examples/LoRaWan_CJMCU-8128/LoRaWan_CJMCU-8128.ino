d8cdce23439555c2abfd8c8c3bddd753651607a2
root <ray@raystream.de>
2019-11-04 17:58:04 +0100

    flags for using the MJMCU-8128 LoRa example with an MJMCU-8128, BME680 or BME280 sensor added TTN Decoders for all off these sensors.

--- libraries/LoRa/examples/LoRaWan_CJMCU-8128/LoRaWan_CJMCU-8128.ino
+++ libraries/LoRa/examples/LoRaWan_CJMCU-8128/LoRaWan_CJMCU-8128.ino
@@ -2,11 +2,27 @@
 #include "Arduino.h"
 #include <Wire.h>
 
+#define MJMCU_8128 1
+#define BME_680    0
+#define BME_280    0
+
+#if(MJMCU_8128 == 1)
+//#include "BH1750.h"
 #include <BMP280.h>
 #include "HDC1080.h"
-//#include <BH1750.h>
 #include <CCS811.h>
 #include <hal/soc/flash.h>
+#endif
+
+#if(BME_680 ==1)
+//#include "BH1750.h"
+#include "BME680.h"
+#endif
+
+#if(BME_280 ==1)
+//#include "BH1750.h"
+#include "BME280.h"
+#endif
 
 //HI
 //AT+DevEui=002307E701EEDF8E
@@ -82,12 +98,23 @@ uint16_t baseline;
 int count;
 int maxtry = 50;
 
+#if(MJMCU_8128 == 1)
 HDC1080 hdc1080;
-//BH1750 lightMeter;
 CCS811 ccs;
 BMP280 bmp;
-//ClosedCube_BME680 bme680;
-//BME280 bme280;
+//BH1750 lightMeter;
+#endif
+
+#if(BME_680 ==1)
+BME680 bme680;
+//BH1750 lightMeter;
+#endif
+
+#if(BME_280 ==1)
+BME280 bme280;
+//BH1750 lightMeter;
+#endif
+
 
 /*!
    \brief   Prepares the payload of the frame
@@ -99,6 +126,10 @@ static void PrepareTxFrame( uint8_t port )
   digitalWrite(Vext, LOW);
   delay(500);
 
+/*
+ *  MJMCU-8128
+ */
+#if(MJMCU_8128 == 1)
   count = 0;
   hdc1080.begin(0x40);
   delay(500);
@@ -258,8 +289,175 @@ static void PrepareTxFrame( uint8_t port )
   Serial.print(baseline);
   Serial.print(", BatteryVoltage:");
   Serial.println(BatteryVoltage);
+#endif
+
+/*
+ * BME680
+ */
+
+#if(BME_680 == 1)
+  //  if (!lightMeter.begin(BH1750::ONE_TIME_HIGH_RES_MODE_2)) {
+  //    Serial.print("Failed to start BH2750!");
+  //  }
+  //  float lux = lightMeter.readLightLevel();
+  //  lightMeter.end();
+  lux = 0.0;
+  bme680.init(0x77); // I2C address: 0x76 or 0x77
+  bme680.reset();
+
+  Serial.print("Chip ID=0x");
+  Serial.println(bme680.getChipID(), HEX);
+
+  // oversampling: humidity = x1, temperature = x2, pressure = x16
+  bme680.setOversampling(BME680_OVERSAMPLING_X1, BME680_OVERSAMPLING_X2, BME680_OVERSAMPLING_X16);
+  bme680.setIIRFilter(BME680_FILTER_3);
+  bme680.setGasOn(300, 100); // 300 degree Celsius and 100 milliseconds
+
+  bme680.setForcedMode();
+  BME680_Status status = readAndPrintStatus();
+  if (status.newDataFlag) {
+    Serial.print("result: ");
+    Temperature = bme680.readTemperature();
+    Pressure = bme680.readPressure();
+    humidity = bme680.readHumidity();
+    TVOC = bme680.readGasResistance();
+
+    Wire.end();
+    digitalWrite(Vext, HIGH);
+    uint16_t BatteryVoltage = GetBatteryVoltage();
+    unsigned char *puc;
+
+    puc = (unsigned char *)(&Temperature);
+    AppDataSize = 22;//AppDataSize max value is 64
+    AppData[0] = puc[0];
+    AppData[1] = puc[1];
+    AppData[2] = puc[2];
+    AppData[3] = puc[3];
+
+    puc = (unsigned char *)(&Humidity);
+    AppData[4] = puc[0];
+    AppData[5] = puc[1];
+    AppData[6] = puc[2];
+    AppData[7] = puc[3];
+
+    puc = (unsigned char *)(&lux);
+    AppData[8] = puc[0];
+    AppData[9] = puc[1];
+    AppData[10] = puc[2];
+    AppData[11] = puc[3];
+
+    puc = (unsigned char *)(&Pressure);
+    AppData[12] = puc[0];
+    AppData[13] = puc[1];
+    AppData[14] = puc[2];
+    AppData[15] = puc[3];
+
+    puc = (unsigned char *)(&tvoc);
+    AppData[20] = puc[0];
+    AppData[21] = puc[1];
+    AppData[22] = puc[2];
+    AppData[23] = puc[3];
+
+    AppData[24] = (uint8_t)(BatteryVoltage >> 8);
+    AppData[25] = (uint8_t)BatteryVoltage;
+
+    Serial.print("T=");
+    Serial.print(Temperature);
+    Serial.print("C, RH=");
+    Serial.print(Humidity);
+    Serial.print("%, Lux=");
+    Serial.print(lux);
+    Serial.print(" lx, Pressure=");
+    Serial.print(Pressure);
+    Serial.print(" hPA, GAS=");
+    Serial.print(tvoc);
+    Serial.print(", BatteryVoltage:");
+    Serial.println(BatteryVoltage);
+
+    bme680.setForcedMode();
+  } else {
+    delay(200); // sensor data not yet ready
+  }
+#endif
+
+/*
+ * BME280
+ */
+#if(BME_280 == 1)
+  if (!bme280.init()) {
+    Serial.println("Device error!");
+  }
+  Temperature = bme280.getTemperature();
+  Pressure = bme280.getPressure();
+  Humidty = bme280.getHumidity()
+
+            //  if (!lightMeter.begin(BH1750::ONE_TIME_HIGH_RES_MODE_2)) {
+            //    Serial.print("Failed to start BH2750!");
+            //  }
+            //  float lux = lightMeter.readLightLevel();
+            //  lightMeter.end();
+            lux = 0.0;
+  Wire.end();
+  digitalWrite(Vext, HIGH);
+  uint16_t BatteryVoltage = GetBatteryVoltage();
+  unsigned char *puc;
+
+  puc = (unsigned char *)(&Temperature);
+  AppDataSize = 18;//AppDataSize max value is 64
+  AppData[0] = puc[0];
+  AppData[1] = puc[1];
+  AppData[2] = puc[2];
+  AppData[3] = puc[3];
+
+  puc = (unsigned char *)(&Humidity);
+  AppData[4] = puc[0];
+  AppData[5] = puc[1];
+  AppData[6] = puc[2];
+  AppData[7] = puc[3];
+
+  puc = (unsigned char *)(&lux);
+  AppData[8] = puc[0];
+  AppData[9] = puc[1];
+  AppData[10] = puc[2];
+  AppData[11] = puc[3];
+
+  puc = (unsigned char *)(&Pressure);
+  AppData[12] = puc[0];
+  AppData[13] = puc[1];
+  AppData[14] = puc[2];
+  AppData[15] = puc[3];
+
+  AppData[24] = (uint8_t)(BatteryVoltage >> 8);
+  AppData[25] = (uint8_t)BatteryVoltage;
+
+  Serial.print("T=");
+  Serial.print(Temperature);
+  Serial.print("C, RH=");
+  Serial.print(Humidity);
+  Serial.print("%, Lux=");
+  Serial.print(lux);
+  Serial.print(" lx, Pressure=");
+  Serial.print(Pressure);
+  Serial.print(" hPA, BatteryVoltage:");
+  Serial.println(BatteryVoltage);
+#endif
 }
 
+#if(BME_680 == 1)
+BME680_Status readAndPrintStatus() {
+  BME680_Status status = bme680.readStatus();
+  Serial.print("status: (");
+  Serial.print(status.newDataFlag);
+  Serial.print(",");
+  Serial.print(status.measuringStatusFlag);
+  Serial.print(",");
+  Serial.print(status.gasMeasuringStatusFlag);
+  Serial.print(",");
+  Serial.print(status.gasMeasurementIndex);
+  Serial.println(") (newDataFlag,StatusFlag,GasFlag,GasIndex)");
+  return status;
+}
+#endif
 
 void setup() {
   BoardInitMcu( );
