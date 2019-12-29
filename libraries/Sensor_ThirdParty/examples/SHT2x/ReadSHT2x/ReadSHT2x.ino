fc31df23cd293e9feb015c91e8565134113b028b
Mike Cochrane <mikec@mikenz.geek.nz>
2019-12-29 20:02:46 +1300

    SHT20 works

--- libraries/Sensor_ThirdParty/examples/SHT2x/ReadSHT2x/ReadSHT2x.ino
+++ libraries/Sensor_ThirdParty/examples/SHT2x/ReadSHT2x/ReadSHT2x.ino
@@ -2,10 +2,6 @@
  * ReadSHT2x
  *  An example sketch that reads the sensor and prints the
  *  relative humidity to the PC's serial port
- *
- *  Tested with:
- *    - SHT21-Breakout Humidity sensor from Modern Device
- *    - SHT2x-Breakout Humidity sensor from MisensO Electronics
  ***************************************************************/
 
 #include <Wire.h>
@@ -14,23 +10,23 @@
 
 void setup()
 {
-  pinMode(Vext, OUTPUT);
-  digitalWrite(Vext, LOW);
-  delay(500);
-
-  Wire.begin();
-  Serial.begin(115200);
+    pinMode(Vext, OUTPUT);
+    Serial.begin(115200);
 }
 
 void loop()
 {
-  Serial.print("Humidity(%RH): ");
-  Serial.print(SHT2x.GetHumidity());
-  Serial.print("     Temperature(C): ");
-  Serial.println(SHT2x.GetTemperature());
-  Serial.print("     Dewpoint(C): ");
-  Serial.println(SHT2x.GetDewPoint());
-  
-  delay(1000);
-}
+    digitalWrite(Vext, LOW);
+    delay(50);
 
+    Wire.begin();
+    Serial.print("Humidity(%RH): ");
+    Serial.print(SHT2x.GetHumidity());
+    Serial.print("     Temperature(C): ");
+    Serial.println(SHT2x.GetTemperature());
+    Wire.end();
+
+    digitalWrite(Vext, HIGH);
+
+    delay(3000);
+}
