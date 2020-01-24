0392b85edf89083b204677e199f3181c60e5fa36
Heltec-Aaron-Lee <support@heltec.cn>
2020-01-24 17:11:33 +0800

    Add LoRa Sender Read Battery voltage example

---
+++ libraries/LoRa/examples/LoRaBasic/LoRaSender_ReadBattery/LoRaSender_ReadBattery.ino
@@ -0,0 +1,146 @@
+/* Heltec Automation Ping Pong communication test example
+ *
+ * Function:
+ * 1. CubeCell read battery voltage and transmit via LoRa.
+ * 
+ * Description:
+ * 1. Only hardware layer communicate, no LoRaWAN protocol support;
+ * 2. This examplese can communicate with other ESP32 LoRa device,
+ *    WiFi LoRa 32, Wireless Stick, Wireless Stick Lite, etc. 
+ * 3. This example is for CubeCell hardware basic test.
+ *
+ * HelTec AutoMation, Chengdu, China
+ * 成都惠利特自动化科技有限公司
+ * https://heltec.org
+ *
+ * this project also realess in GitHub:
+ * https://github.com/HelTecAutomation/ASR650x-Arduino
+ * */
+
+#include "LoRaWan_APP.h"
+#include "Arduino.h"
+
+/*
+ * set LoraWan_RGB to 1,the RGB active in loraWan
+ * RGB red means sending;
+ * RGB green means received done;
+ */
+#ifndef LoraWan_RGB
+#define LoraWan_RGB 0
+#endif
+
+#define RF_FREQUENCY                                868000000 // Hz
+
+#define TX_OUTPUT_POWER                             14        // dBm
+
+#define LORA_BANDWIDTH                              0         // [0: 125 kHz,
+                                                              //  1: 250 kHz,
+                                                              //  2: 500 kHz,
+                                                              //  3: Reserved]
+#define LORA_SPREADING_FACTOR                       8         // [SF7..SF12]
+#define LORA_CODINGRATE                             4         // [1: 4/5,
+                                                              //  2: 4/6,
+                                                              //  3: 4/7,
+                                                              //  4: 4/8]
+#define LORA_PREAMBLE_LENGTH                        8         // Same for Tx and Rx
+#define LORA_SYMBOL_TIMEOUT                         0         // Symbols
+#define LORA_FIX_LENGTH_PAYLOAD_ON                  false
+#define LORA_IQ_INVERSION_ON                        false
+
+
+#define RX_TIMEOUT_VALUE                            1000
+#define BUFFER_SIZE                                 30 // Define the payload size here
+
+char txpacket[BUFFER_SIZE];
+
+static RadioEvents_t RadioEvents;
+void OnTxDone( void );
+void OnTxTimeout( void );
+
+
+
+typedef enum
+{
+    LOWPOWER,
+    ReadVoltage,
+    TX
+}States_t;
+
+States_t state;
+bool sleepmode = false;
+int16_t RSSI,rxSize;
+uint16_t voltage;
+
+void setup() {
+    BoardInitMcu( );
+    Serial.begin(115200);
+
+    // txnumber=0;
+    voltage = 0;
+    RSSI=0;
+
+    RadioEvents.TxDone = OnTxDone;
+    RadioEvents.TxTimeout = OnTxTimeout;
+
+    Radio.Init( &RadioEvents );
+    Radio.SetChannel( RF_FREQUENCY );
+    Radio.SetTxConfig( MODEM_LORA, TX_OUTPUT_POWER, 0, LORA_BANDWIDTH,
+                                   LORA_SPREADING_FACTOR, LORA_CODINGRATE,
+                                   LORA_PREAMBLE_LENGTH, LORA_FIX_LENGTH_PAYLOAD_ON,
+                                   true, 0, 0, LORA_IQ_INVERSION_ON, 3000 );
+
+    state=TX;
+}
+
+
+
+void loop()
+{
+  switch(state)
+  {
+    case TX:
+    {
+      sprintf(txpacket,"%s","ADC_battery: ");
+      sprintf(txpacket+strlen(txpacket),"%d",voltage);
+      RGB_ON(COLOR_SEND,0);
+      Serial.printf("\r\nsending packet \"%s\" , length %d\r\n",txpacket, strlen(txpacket));
+      Radio.Send( (uint8_t *)txpacket, strlen(txpacket) );
+      state=LOWPOWER;
+      break;
+    }
+    case LOWPOWER:
+    {
+      LowPower_Handler();
+      delay(100);
+      RGB_OFF();
+      delay(2000);  //LowPower time
+      state = ReadVoltage; 
+      break;
+    }
+    case ReadVoltage:
+    {
+      pinMode(ADC_CTL,OUTPUT);
+      digitalWrite(ADC_CTL,LOW);
+      voltage=analogRead(ADC)*2;
+      digitalWrite(ADC_CTL,HIGH);
+      state = TX;
+      break;
+    }
+     default:
+          break;
+  }
+}
+
+void OnTxDone( void )
+{
+  Serial.print("TX done!");
+  RGB_ON(0,0);
+  state=TX;
+}
+
+void OnTxTimeout( void )
+{
+    Radio.Sleep( );
+    Serial.print("TX Timeout......");
+    state=TX;
+}
