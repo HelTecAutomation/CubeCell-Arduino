0bf06a2a8c84e1ef36a9e7e44a49b2041a777c2e
Heltec-Aaron-Lee <support@heltec.cn>
2020-03-19 12:28:00 +0800

    added tx power test example

---
+++ libraries/LoRa/examples/LoRaBasic/TxPowerTest/TxPowerTest.ino
@@ -0,0 +1,26 @@
+#include "radio.h"
+#include "Arduino.h"
+
+#define RF_FREQUENCY                                868000000 // Hz
+#define TX_OUTPUT_POWER                             20        // 20 dBm
+#define TX_TIMEOUT                                  10        // seconds (MAX value)
+static RadioEvents_t RadioEvents;
+
+void OnRadioTxTimeout( void )
+{
+    // Restarts continuous wave transmission when timeout expires
+    Radio.SetTxContinuousWave( RF_FREQUENCY, TX_OUTPUT_POWER, TX_TIMEOUT );
+}
+void setup() {
+  boardInitMcu();
+  // put your setup code here, to run once:
+  RadioEvents.TxTimeout = OnRadioTxTimeout;
+  Radio.Init( &RadioEvents );
+
+  Radio.SetTxContinuousWave( RF_FREQUENCY, TX_OUTPUT_POWER, TX_TIMEOUT );
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
