53409c24aaf849f732005777f67d0e837b731606
Heltec-Aaron-Lee <support@heltec.cn>
2020-04-03 18:18:03 +0800

    Rename Adafruit NeoPixel library

--- libraries/RGB/src/CubeCell_NeoPixel.h
+++ libraries/RGB/src/CubeCell_NeoPixel.h
@@ -1,38 +1,3 @@
-/*!
- * @file CubeCell_NeoPixel.h
- *
- * This is part of Adafruit's NeoPixel library for the Arduino platform,
- * allowing a broad range of microcontroller boards (most AVR boards,
- * many ARM devices, ESP8266 and ESP32, among others) to control Adafruit
- * NeoPixels, FLORA RGB Smart Pixels and compatible devices -- WS2811,
- * WS2812, WS2812B, SK6812, etc.
- *
- * Adafruit invests time and resources providing this open source code,
- * please support Adafruit and open-source hardware by purchasing products
- * from Adafruit!
- *
- * Written by Phil "Paint Your Dragon" Burgess for Adafruit Industries,
- * with contributions by PJRC, Michael Miller and other members of the
- * open source community.
- *
- * This file is part of the CubeCell_NeoPixel library.
- *
- * CubeCell_NeoPixel is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * CubeCell_NeoPixel is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with NeoPixel.  If not, see
- * <http://www.gnu.org/licenses/>.
- *
- */
-
 #ifndef CubeCell_NeoPixel_H
 #define CubeCell_NeoPixel_H
 
