98565237c2174cf5cca96d95edc89614d4ffb91a
Heltec-Aaron-Lee <support@heltec.cn>
2021-02-04 18:02:48 +0800

    updated example AT_command.ino

--- libraries/LoRa/examples/AT_Command/AT_Command.ino
+++ libraries/LoRa/examples/AT_Command/AT_Command.ino
@@ -91,11 +91,15 @@ void setup() {
 	Serial.begin(115200);
 	passthroughMode = true;
 	enableAt(); 
+#if(LORAWAN_DEVEUI_AUTO)
+	LoRaWAN.generateDeveuiByChipID();
+#endif
 	getDevParam();
 	printDevParam();
 	deviceState = DEVICE_STATE_SLEEP;
 	LoRaWAN.ifskipjoin();
 	deviceState_lora = LORA_INIT;
+	delay(5);
 }
 
 void loop()
