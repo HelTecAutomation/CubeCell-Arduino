f4a303b583cf3cbf290175244ecb92ef895dca5c
jke <wart,1972>
2021-02-28 17:43:43 +0100

    macros based on architecture not specific mcu

--- cores/asr650x/loramac/mac/region/RegionAU915.c
+++ cores/asr650x/loramac/mac/region/RegionAU915.c
@@ -27,7 +27,7 @@ Maintainer: Miguel Luis ( Semtech ), Gregory Cristian ( Semtech ) and Daniel Jae
 #include <math.h>
 
 #include "radio.h"
-#if defined(__ASR6501__)
+#if defined(__asr650x__)
 #include "timeServer.h"
 #else
 #include "timer.h"
