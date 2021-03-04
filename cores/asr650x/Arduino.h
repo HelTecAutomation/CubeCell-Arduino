df456a9e929291978c90b991896bc41e08db96c6
Heltec-Aaron-Lee <support@heltec.cn>
2021-03-04 11:56:37 +0800

    declared function randseed()

--- cores/asr650x/Arduino.h
+++ cores/asr650x/Arduino.h
@@ -115,6 +115,8 @@ uint8_t shiftIn(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder);
 void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint8_t val);
 
 long random(long howbig);
+void randomSeed(unsigned long seed);
+long random(long howsmall, long howbig);
 
 /* system functions */
 int main( void );
