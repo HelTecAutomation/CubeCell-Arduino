71bb8df1c5be1319622f6222bbb36cf9e615907a
Heltec-Aaron-Lee <support@heltec.cn>
2021-05-20 15:54:44 +0800

    updated asr6601 lib

--- libraries/LoraWan102/src/loramac/Commissioning.h
+++ libraries/LoraWan102/src/loramac/Commissioning.h
@@ -27,7 +27,7 @@
  * User application data buffer size
  */
 #if defined(__ASR6601__)
-#define LORAWAN_APP_DATA_MAX_SIZE                   256 //if use AT mode, don't modify this value or may run dead
+#define LORAWAN_APP_DATA_MAX_SIZE                   255 //if use AT mode, don't modify this value or may run dead
 #else
 #define LORAWAN_APP_DATA_MAX_SIZE                   128 //if use AT mode, don't modify this value or may run dead
 #endif
