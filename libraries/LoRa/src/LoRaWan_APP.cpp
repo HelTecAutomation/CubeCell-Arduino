44aacee147f510de4ca68ad8ede27ef00b652e34
Heltec-Aaron-Lee <support@heltec.cn>
2023-03-13 15:19:07 +0800

    update lorawan_oled

--- libraries/LoRa/src/LoRaWan_APP.cpp
+++ libraries/LoRa/src/LoRaWan_APP.cpp
@@ -293,6 +293,7 @@ void __attribute__((weak)) downLinkDataHandle(McpsIndication_t *mcpsIndication)
  *               containing indication attributes.
  */
 int revrssi;
+int revsnr;
 static void McpsIndication( McpsIndication_t *mcpsIndication )
 {
 	if( mcpsIndication->Status != LORAMAC_EVENT_INFO_STATUS_OK )
@@ -302,6 +303,7 @@ static void McpsIndication( McpsIndication_t *mcpsIndication )
 #if defined(CubeCell_BoardPlus)||defined(CubeCell_GPS)
 	ifDisplayAck=1;
 	revrssi=mcpsIndication->Rssi;
+	revsnr=mcpsIndication->Snr;
 #endif
 #if (LoraWan_RGB==1)
 	turnOnRGB(COLOR_RECEIVED, 200);
@@ -771,8 +773,8 @@ void LoRaWanClass::displayAck()
     ifDisplayAck--;
 	display.clear();
 	display.drawString(64, 22, "ACK RECEIVED");
-	char temp[10];
-	sprintf(temp,"rssi: %d ",revrssi);
+	char temp[30];
+	sprintf(temp,"rssi: %d, snr: %d ",revrssi,revsnr);
 	display.setFont(ArialMT_Plain_10);
 	display.setTextAlignment(TEXT_ALIGN_RIGHT);
 	display.drawString(128, 0, temp);
